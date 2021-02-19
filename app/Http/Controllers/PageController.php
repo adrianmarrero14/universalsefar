245a8a280ee37887e08c9aa0557094f829c7ed54
Adrian <adrianmarrero18@gmail.com>
2021-02-18 21:36:13 -0400

    Creados Controller de Documentos y Arbol --resource

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function documentos()
-    {
-        return view('documentos');
-    }
-
     public function biblioteca()
     { 
         return view('biblioteca');
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', 'HomeController@index')->middleware('auth')->name('home');
 
 Route::get('/home', 'HomeController@index')->middleware('auth')->name('home');
 
-Route::get('/documentos', 'PageController@documentos')->middleware('auth')->name('documentos');
+Route::get('/documentos', 'DocumentosController@index')->middleware('auth')->name('documentos');
 
 Route::get('/biblioteca', 'PageController@biblioteca')->middleware('auth')->name('biblioteca');
 
