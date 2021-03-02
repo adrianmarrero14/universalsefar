03eebc7bb446480cbc441ba3eada0236e01b2a72
Adrian <adrianmarrero18@gmail.com>
2021-03-02 12:01:19 -0400

    Titulos adaptados

--- resources/views/arbol-clientes/index.blade.php
+++ resources/views/arbol-clientes/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Árbol Clientes</title>
 
         <!-- Styles Component -->
         <x-styles />
--- resources/views/casos-finalizados/show.blade.php
+++ resources/views/casos-finalizados/show.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Caso Finalizado | Detalle</title>
 
     <!-- Styles Component -->
     <x-styles />
